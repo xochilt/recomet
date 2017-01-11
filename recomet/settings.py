9e811aca2c71a8849f5c4a74d7d59d008346b741
Xochilt Ramirez <xochilt.ramirez@gmail.com>
2017-01-11 10:39:08 -0800

    settings updated.

--- recomet/settings.py
+++ recomet/settings.py
@@ -12,25 +12,15 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*(5n6i@rj8&5o9meua7s2gx&75fs!dw0r4f!+0!=y)hot)-5yn'
+SECRET_KEY = ''
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
 # Application definition
-
-#CRISPY_TEMPLATE_PACK='bootstrap3'
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,10 +33,9 @@ INSTALLED_APPS = (
     'django_countries',
 )
 
-ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
+ACCOUNT_ACTIVATION_DAYS = 7 
 ABSOLUTE_URL_OVERRIDES = {
     'restaurant.index': lambda o: "/recomet/%s/",
-    #'news.story': lambda o: "/stories/%s/%s/" % (o.pub_year, o.slug),
 }
 REGISTRATION_OPEN=True
 
@@ -61,19 +50,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'recomet.urls'
-
 WSGI_APPLICATION = 'recomet.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
 
 DATABASES = {
     'default': {
@@ -88,37 +65,25 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = False
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
 
 
-#CONFIGURACION AGREGADA AL SETTINGS ORIGINAL...................................
+#CONFIGURACION AGREGADA AL SETTINGS
 
 STATIC_ROOT ='/static'
-
-#MEDIA_ROOT = '/static/'
-
 STATICFILES_DIRS = (
     '',
     )
 
 # Make this unique, and don't share it with anybody. WEATHER.
-SECRET_KEY = '9on5y307x3=e9^wm=n_pney-(*-#a(!tn3&&1)cg1!%90=+0p^'
-
+SECRET_KEY = ''
 
 # List of callables that know how to import templates from various sources.
 #TEMPLATE_LOADERS = (
@@ -126,43 +91,19 @@ SECRET_KEY = '9on5y307x3=e9^wm=n_pney-(*-#a(!tn3&&1)cg1!%90=+0p^'
 #    'django.template.loaders.app_directories.load_template_source',
 #    #'django.template.loaders.eggs.load_template_source',
 #)
-
 TEMPLATE_DIRS = (
     '/root/recomet/templates',
-    #os.path.join.dirname(BASE_DIR), "static", "templates",
     )
 
 LOGIN_URL = '/recomet'
-
-#PARA REDIRECCIONAR...
 LOGIN_REDIRECT_URL = '/recomet'
-
-AUTH_PROFILE_MODULE = 'restaurant.UserProfile'
-
-
-
-#para bootstrap
-
-#BOOTSTRAP3_FOLDER = '/bootstrap3/'
-
- #""" bad_domain=['aim.com', 'aol.com', 'email.com', 'gmail.com',
- #                  'googlemail.com', 'hotmail.com', 'hushmail.com',
- #                  'msn.com', 'mail.ru', 'mailinator.com', 'live.com',
- #                  'yahoo.com']"""
- 
- 
- 
+AUTH_PROFILE_MODULE = 'restaurant.UserProfile' 
 EMAIL_HOST = 'smtp.gmail.com'
-
 EMAIL_PORT = 587
-
-EMAIL_HOST_USER = 'xochilt.ramirez@gmail.com'
-
-EMAIL_HOST_PASSWORD = 'daeqocohhicmhkvu'   #passwd de apps de google
-
+EMAIL_HOST_USER = 'x@gmail.com'
+EMAIL_HOST_PASSWORD = ''  
 EMAIL_USE_TLS = True
-
-DEFAULT_FROM_EMAIL = 'xochilt.ramirez@gmail.com'
+DEFAULT_FROM_EMAIL = 'x@gmail.com'
 
  
  
